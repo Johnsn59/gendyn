53cd0f1c4cfa1c5fad6d8e476689dddcc630821e
Lane Willmore <57517913+Tech-Lane@users.noreply.github.com>
2020-04-10 02:44:23 -0700

    may revert

--- PutAVettoWork.Site/Controllers/EventPostsController.cs
+++ PutAVettoWork.Site/Controllers/EventPostsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PutAVettoWork.Site.Data;
+using PutAVettoWork.Site.Models;
 
 namespace PutAVettoWork.Site.Controllers
 {
@@ -22,7 +23,19 @@ namespace PutAVettoWork.Site.Controllers
         public async Task<IActionResult> Index()
         {
 
-            return View(await context.EventPosts.OrderByDescending(x => x.Id).ToListAsync());
+            return View("_SecondaryLayout.cshtml",await context.EventPosts.OrderByDescending(x => x.Id).ToListAsync());
+        }
+
+        // GET /eventposts/id
+        public async Task<IActionResult> Eventpost(int id)
+        {
+            EventPost eventPost = await context.EventPosts.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+            if (eventPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventPost);
         }
     }
 }
